783a465eb4ec2ecbac5094f3f96e2c7e42b29c93
ggotchaa <nurbekenov.nb@gmail.com>
2020-09-21 18:32:16 +0800

    Add latest build

---
+++ build/precache-manifest.d31a0f7241b03854a6166a7c3f366f70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df88fa591928a64dce1b",
+    "url": "/static/css/main.4124a9f0.chunk.css"
+  },
+  {
+    "revision": "df88fa591928a64dce1b",
+    "url": "/static/js/main.df88fa59.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8e3938f92a9e9f5f7e70",
+    "url": "/static/js/2.8e3938f9.chunk.js"
+  },
+  {
+    "revision": "37f9718513884d90dc0a0165ec718924",
+    "url": "/index.html"
+  }
+];
